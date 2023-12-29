5305b1e70816839f4d4fab63465024d031b3e1fe
Nilaxsh <nilaxsakana@gmail.com>
2023-12-29 15:07:10 +0530

    weatherapp

---
+++ src/componants/weather.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './styles.css';
+import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+
+const refresh = () => {
+  window.location.reload();
+}
+
+// REACT_APP_API_URL = https://api.openweathermap.org/data/2.5
+// REACT_APP_API_KEY = 7b8b2837f9ad06710cccdbb9fb9efd44
+// REACT_APP_ICON_URL = https://openweathermap.org/img/w
+
+const WeatherCard = ({weatherData}) => (
+  <div className="main">
+      <p className="header">{weatherData.name}</p>
+      <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+      <div className="flex">
+        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+        <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
+    
+  </div>
+)
+
+export default WeatherCard;
+
+      
